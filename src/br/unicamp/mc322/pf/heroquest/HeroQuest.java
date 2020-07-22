a8c5e1bf35f344098498e74656f9a64963c44be7
Hik <luiz.alves@playkids.com>
2020-07-22 02:35:53 -0300

    Refactor app entrypoint

--- src/br/unicamp/mc322/pf/heroquest/HeroQuest.java
+++ src/br/unicamp/mc322/pf/heroquest/HeroQuest.java
@@ -1,11 +1,9 @@
 package br.unicamp.mc322.pf.heroquest;
 
-import java.util.Scanner;
-
 import br.unicamp.mc322.pf.heroquest.render.*;
-import br.unicamp.mc322.pf.heroquest.dice.*;
 import br.unicamp.mc322.pf.heroquest.map.*;
 import br.unicamp.mc322.pf.heroquest.map.generation.ClassicalMapGenerator;
+import br.unicamp.mc322.pf.heroquest.map.generation.FileMapGenerator;
 import br.unicamp.mc322.pf.heroquest.map.illuminator.BasicIlluminator;
 
 public class HeroQuest {
@@ -17,25 +15,11 @@ public class HeroQuest {
 	
 	public void start() {
 		Map world = new Map(new ClassicalMapGenerator(), new BasicIlluminator());
+		//Map world = new Map(new FileMapGenerator(), new BasicIlluminator());
 		renderer.setMap(world);
 		renderer.renderWorld();
 				
 		while (true) {
 		}
-	}
-	
-	public static void main(String[] args) {		
-		// Print loaded map
-		String path = "map_files/example.txt";
-        try {
-        	char[][] map = MapFileReader.readFile(path);
-        	for(int i = 0; i < 10; i++) {
-        		for(int j = 0; j < 10; j++)
-        			System.out.print(map[i][j]);
-        		System.out.println();
-        	}
-        } catch(Exception exception) {
-        	System.out.println(exception);
-        }
 	}
 }
