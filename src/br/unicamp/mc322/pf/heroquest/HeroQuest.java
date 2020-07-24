f9faed553203d7746b31b881767a07ef3a41504a
Hik <luiz.alves@playkids.com>
2020-07-24 00:22:42 -0300

    Refactor game loop

--- src/br/unicamp/mc322/pf/heroquest/HeroQuest.java
+++ src/br/unicamp/mc322/pf/heroquest/HeroQuest.java
@@ -1,25 +1,38 @@
 package br.unicamp.mc322.pf.heroquest;
 
 import br.unicamp.mc322.pf.heroquest.render.*;
+import br.unicamp.mc322.pf.heroquest.gameobject.entity.EntityManager;
 import br.unicamp.mc322.pf.heroquest.map.*;
 import br.unicamp.mc322.pf.heroquest.map.generation.ClassicalMapGenerator;
 import br.unicamp.mc322.pf.heroquest.map.generation.FileMapGenerator;
 import br.unicamp.mc322.pf.heroquest.map.illuminator.BasicIlluminator;
 
 public class HeroQuest {
-	Renderer renderer;
+	private Renderer renderer;
+	private EntityManager entityManager;
 	
 	public HeroQuest(Renderer renderer) {
 		this.renderer = renderer;
 	}
 	
 	public void start() {
-		Map world = new Map(new ClassicalMapGenerator(), new BasicIlluminator());
+		entityManager = new EntityManager();
+		Map world = new Map(new ClassicalMapGenerator(), new BasicIlluminator(), entityManager);
 		//Map world = new Map(new FileMapGenerator(), new BasicIlluminator());
 		renderer.setMap(world);
 		renderer.renderWorld();
 				
+		runGameLoop();
+	}
+	
+	private void runGameLoop() {
 		while (true) {
+			entityManager.nextTurn();
+			
+			// TODO: Maybe remove this delay. It´s ugly
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) { }
 		}
 	}
 }
