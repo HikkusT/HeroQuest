cdc570fcccc00eb9b8eca36b11f83740cc33fdfa
Hik <luiz.alves@playkids.com>
2020-07-25 18:15:18 -0300

    Create player strategy

---
+++ src/br/unicamp/mc322/pf/heroquest/gameobject/entity/strategy/PlayerStrategy.java
@@ -0,0 +1,39 @@
+package br.unicamp.mc322.pf.heroquest.gameobject.entity.strategy;
+
+import br.unicamp.mc322.pf.heroquest.HeroQuest;
+import br.unicamp.mc322.pf.heroquest.gameobject.entity.hero.Hero;
+import br.unicamp.mc322.pf.heroquest.input.Command;
+import br.unicamp.mc322.pf.heroquest.input.Input;
+import br.unicamp.mc322.pf.heroquest.utils.Direction;
+
+public class PlayerStrategy implements TurnStrategy {
+	Hero player;
+	Input input;
+	
+	public PlayerStrategy(Hero player) {
+		this.player = player;
+		this.input = HeroQuest.getInstance().getInput();
+	}
+
+	@Override
+	public void execute() {
+		Command command = input.waitForNextCommand();
+		switch (command) {
+		case MOVE_UP:
+			player.move(Direction.NORTH);
+			break;
+		case MOVE_RIGHT:
+			player.move(Direction.EAST);
+			break;
+		case MOVE_DOWN:
+			player.move(Direction.SOUTH);
+			break;
+		case MOVE_LEFT:
+			player.move(Direction.WEST);
+			break;
+		default:
+			break;
+			
+		}
+	}
+}
