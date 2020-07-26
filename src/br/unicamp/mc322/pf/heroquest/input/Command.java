132b9c3d89330117dab4247bcd04199837392807
Hik <luiz.alves@playkids.com>
2020-07-26 11:43:40 -0300

    Add helper funcions to command

--- src/br/unicamp/mc322/pf/heroquest/input/Command.java
+++ src/br/unicamp/mc322/pf/heroquest/input/Command.java
@@ -5,10 +5,29 @@ public enum Command {
 	MOVE_RIGHT,
 	MOVE_DOWN,
 	MOVE_LEFT,
-	OPEN_DOOR;
+	OPEN_DOOR,
+	SEARCH_TREASURES,
+	SEARCH_TRAPS,
+	DISARM_TRAP,
+	ATTACK,
+	CAST;
 	
-	public static boolean isMovementCommand(Command command) {
-		if (command == MOVE_UP || command == MOVE_RIGHT || command == MOVE_DOWN || command == MOVE_LEFT)
+	public boolean isMovementCommand() {
+		if (this == MOVE_UP || this == MOVE_RIGHT || this == MOVE_DOWN || this == MOVE_LEFT)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isActionCommand() {
+		if (this == SEARCH_TREASURES || this == SEARCH_TRAPS || this == DISARM_TRAP || this == ATTACK || this == CAST)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isCombatCommand() {
+		if (this == ATTACK || this == CAST)
 			return true;
 		else
 			return false;
